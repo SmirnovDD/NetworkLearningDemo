95e98541f2be678ce6e339c25560b6114e36e244
SmirnovDD <123432145@bk.ru>
2021-08-21 18:17:20 +0300

    preparation for unit tests

---
+++ Assets/_Demo/Scripts/Tests/TestMovementController.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestMovementController
+    {
+        [Test]
+        public void Whenact_Andarrange_Thenassert()
+        {
+
+            // Arrange.
+
+            // Act.
+
+            // Assert.
+
+            
+        }
+    }
+}
